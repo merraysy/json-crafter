896eebc285f0a890a3e5d1dad0752e18b4c251f7
Mohammed Erraysy <merraysy@gmail.com>
2017-02-07 22:56:38 +0000

    Build ui markup and styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 
-// components
-import { MenuItem, SplitButton } from 'react-bootstrap';
-
 class App extends Component {
   render() {
+    return null;
     return (
       <div className="container">
         <h1>JSON Craftman!</h1>
-
-        <SplitButton title="Test React Bootstrap" pullRight id="split-dropdown">
-          <MenuItem eventKey="1">It works!</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey="4">Let's do this.</MenuItem>
-        </SplitButton>
       </div>
     );
   }
