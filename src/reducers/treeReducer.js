a62cea13bf7c1bbec53c732ac2429d157cea53cc
Mohammed Erraysy <merraysy@gmail.com>
2017-02-09 11:57:54 +0000

    Remove the removed parent's level as well

--- src/reducers/treeReducer.js
+++ src/reducers/treeReducer.js
@@ -22,14 +22,12 @@ export default (state = initialState, action) => {
           return item;
         });
       return state.set('items', itemsWithAddedItem);
-      break;
     case actionTypes.SAVE_ITEM:
       console.log(action.type);
       const savedItem = state.get('items').find((item) => item.id === action.payload.id);
       const savedItemIndex = state.get('items').indexOf(savedItem);
       const itemsWithSavedItem = state.get('items').set(savedItemIndex, Object.assign({}, savedItem, action.payload));
       return state.set('items', itemsWithSavedItem);
-      break;
     case actionTypes.REMOVE_ITEM:
       console.log(action.type);
       const itemsWithoutRemovedItem = state.get('items')
@@ -39,27 +37,27 @@ export default (state = initialState, action) => {
           if (!foundItem) item.hasChildren = false;
           return item;
         });
-      return state.set('items', itemsWithoutRemoved);
-      break;
+      const levelsWithoutRemovedParent = state.get('levels')
+        .filter((level) => level.parentId !== action.payload.id);
+      return state
+        .set('items', itemsWithoutRemovedItem)
+        .set('levels', levelsWithoutRemovedParent);
     case actionTypes.START_EDITING:
       console.log(action.type);
       const selectedItem = state.get('items').find((item) => item.id === action.payload.id);
       const selectedItemIndex = state.get('items').indexOf(selectedItem);
       const itemsWithSelectedItem = state.get('items').set(selectedItemIndex, Object.assign({}, selectedItem, { isEditing: true }));
       return state.set('items', itemsWithSelectedItem);
-      break;
     case actionTypes.END_EDITING:
       console.log(action.type);
       const editedItem = state.get('items').find((item) => item.id === action.payload.id);
       const editedItemIndex = state.get('items').indexOf(editedItem);
       const itemsWithEditedItem = state.get('items').set(editedItemIndex, Object.assign({}, editedItem, { isEditing: false }));
       return state.set('items', itemsWithEditedItem);
-      break;
     case actionTypes.OPEN_ITEM:
       console.log(action.type);
       const newIndex = action.payload.levelIndex + 1;
       return state.set('levels', state.get('levels').set(newIndex, { index: newIndex, parentId: action.payload.id }));
-      break;
     default:
       return state;
   }
